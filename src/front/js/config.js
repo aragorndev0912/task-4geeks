454b57bef44dc81547f985aa18a5f3910bc96c23
Aragorn <aragorn.dev0912@gmail.com>
2022-07-30 18:09:58 +0000

    fixed

--- src/front/js/config.js
+++ src/front/js/config.js
@@ -1,5 +1,5 @@
 export default {
   hostname:
     process.env.BACKEND_URL ||
-    "https://3001-4geeksacade-reactflaskh-y2lv5dujel0.ws-eu54.gitpod.io",
+    "https://3001-4geeksacade-reactflaskh-rw6uf9r9j6b.ws-eu54.gitpod.io",
 };
