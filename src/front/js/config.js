89f78963b4aada6aa7e12b83d3c5decd2be3fb56
Aragorn <aragorn.dev0912@gmail.com>
2022-07-31 07:59:32 +0000

    fix

--- src/front/js/config.js
+++ src/front/js/config.js
@@ -1,6 +1,7 @@
-const _back_url =
+const _dev =
   "https://3001-4geeksacade-reactflaskh-rw6uf9r9j6b.ws-eu54.gitpod.io";
+const _prod = "https://git.heroku.com/task-4geeks.git";
 
 export default {
-  hostname: `${process.env.BACKEND_URL || _back_url}/api`,
+  hostname: `${_prod}/api`,
 };
